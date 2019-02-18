d7a0e15aa8ff6205abc1bbbdd252e46b7e944bb8
Mutakoduru Sainath <Mutakodurusainath@gmail.com>
2019-02-17 19:25:24 -0500

    changed to configuration annotation

--- src/main/java/com/sai/config/CassandraConfig.java
+++ src/main/java/com/sai/config/CassandraConfig.java
@@ -3,42 +3,42 @@ package com.sai.config;
 import java.util.Collections;
 import java.util.List;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
 import org.springframework.data.cassandra.config.SchemaAction;
 import org.springframework.data.cassandra.core.cql.keyspace.CreateKeyspaceSpecification;
 import org.springframework.data.cassandra.core.cql.keyspace.DropKeyspaceSpecification;
 
-@EnableAutoConfiguration
+@Configuration
 public class CassandraConfig extends AbstractCassandraConfiguration {
 
-    private static final String KEYSPACE = "sample";
+	private static final String KEYSPACE = "sample";
 
-    @Override
-    public SchemaAction getSchemaAction() {
-        return SchemaAction.CREATE_IF_NOT_EXISTS;
-    }
+	@Override
+	public SchemaAction getSchemaAction() {
+		return SchemaAction.CREATE_IF_NOT_EXISTS;
+	}
 
-    @Override
-    protected List<CreateKeyspaceSpecification> getKeyspaceCreations() {
-        CreateKeyspaceSpecification specification = CreateKeyspaceSpecification.createKeyspace(KEYSPACE);
+	@Override
+	protected List<CreateKeyspaceSpecification> getKeyspaceCreations() {
+		CreateKeyspaceSpecification specification = CreateKeyspaceSpecification.createKeyspace(KEYSPACE).ifNotExists();
 
-        return Collections.singletonList(specification);
-    }
+		return Collections.singletonList(specification);
+	}
 
-    @Override
-    protected List<DropKeyspaceSpecification> getKeyspaceDrops() {
-        return Collections.singletonList(DropKeyspaceSpecification.dropKeyspace(KEYSPACE));
-    }
+	@Override
+	protected List<DropKeyspaceSpecification> getKeyspaceDrops() {
+		return Collections.singletonList(DropKeyspaceSpecification.dropKeyspace(KEYSPACE));
+	}
 
-    @Override
-    protected String getKeyspaceName() {
-        return KEYSPACE;
-    }
+	@Override
+	protected String getKeyspaceName() {
+		return KEYSPACE;
+	}
 
-    @Override
-    public String[] getEntityBasePackages() {
-        return new String[]{"com.sai.model"};
-    }
+	@Override
+	public String[] getEntityBasePackages() {
+		return new String[] { "com.sai.model" };
+	}
 
 }
