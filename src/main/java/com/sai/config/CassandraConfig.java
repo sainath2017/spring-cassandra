3c919925b7487d65b6a7d9d2bf069fc5a59ba569
Mutakoduru Sainath <Mutakodurusainath@gmail.com>
2019-02-17 19:46:47 -0500

    removed drop keyspace for every session

--- src/main/java/com/sai/config/CassandraConfig.java
+++ src/main/java/com/sai/config/CassandraConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
 import org.springframework.data.cassandra.config.SchemaAction;
 import org.springframework.data.cassandra.core.cql.keyspace.CreateKeyspaceSpecification;
-import org.springframework.data.cassandra.core.cql.keyspace.DropKeyspaceSpecification;
 
 @Configuration
 public class CassandraConfig extends AbstractCassandraConfiguration {
@@ -24,11 +23,6 @@ public class CassandraConfig extends AbstractCassandraConfiguration {
 		CreateKeyspaceSpecification specification = CreateKeyspaceSpecification.createKeyspace(KEYSPACE).ifNotExists();
 
 		return Collections.singletonList(specification);
-	}
-
-	@Override
-	protected List<DropKeyspaceSpecification> getKeyspaceDrops() {
-		return Collections.singletonList(DropKeyspaceSpecification.dropKeyspace(KEYSPACE));
 	}
 
 	@Override
